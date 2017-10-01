68e5c4eaae5e208c4f3fa20d07d4db50f842c362
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-10-02 00:49:03 +0300

    Student struct

---
+++ CertificationTraining/Workspace/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace CertificationTraining.Workspace
+{
+    public struct Student
+    {
+        public string firstName;
+        public string lastName;
+        public double score1;
+
+        public Student(string first, string last)
+        {
+            this.firstName = first;
+            this.lastName = last;
+
+            this.score1 = 1; // all fields must be initialised *in the constructor*
+
+            AssignScore1(); // it can only be called once all the fields are assigned
+        }
+
+        public void AssignScore1()
+        {
+            this.score1 = 2;
+        }
+    }
+}
