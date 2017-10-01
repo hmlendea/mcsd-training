b73f0604ebd8fde3812ab11fdcc47e5ebd361447
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-10-01 23:55:03 +0300

    Created the solution

---
+++ CertificationTraining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CertificationTraining
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
