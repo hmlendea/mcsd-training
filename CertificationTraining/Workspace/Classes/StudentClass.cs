ccff6a76c1117bf76f8d1f1f350ab1042565e448
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-10-02 21:48:10 +0300

    Property samples

--- CertificationTraining/Workspace/Classes/StudentClass.cs
+++ CertificationTraining/Workspace/Classes/StudentClass.cs
@@ -1,16 +1,71 @@
-﻿namespace CertificationTraining.Workspace.Classes
+﻿using System;
+
+namespace CertificationTraining.Workspace.Classes
 {
     public class StudentClass
     {
         public static int StudentCount;
 
-        public string FirstName;
+        public string firstName;
 
-        public string LastName;
+        public string lastName;
 
-        public string SchoolName;
+        public string schoolName;
 
-        public int Grade;
+        public int grade;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string SchoolName
+        {
+            get
+            {
+                return schoolName;
+            }
+            set
+            {
+                schoolName = value;
+            }
+        }
+
+        public int Grade
+        {
+            get
+            {
+                return grade;
+            }
+            set
+            {
+                if (value < 0 || value > 10)
+                {
+                    throw new ArgumentException($"Invalid value for '{nameof(Grade)}'");
+                }
+
+                grade = value;
+            }
+        }
 
         public StudentClass()
         {
@@ -18,30 +73,30 @@
 
         public StudentClass(string firstName, string lastName, string schoolName)
         {
-            FirstName = firstName;
-            LastName = lastName;
-            SchoolName = schoolName;
-            Grade = -1;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.schoolName = schoolName;
+            grade = -1;
         }
 
         public StudentClass(string firstName, string lastName, string schoolName, int grade)
         {
-            FirstName = firstName;
-            LastName = lastName;
-            SchoolName = schoolName;
-            Grade = grade;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.schoolName = schoolName;
+            this.grade = grade;
         }
 
         // SAMPLE: Class methods - implementation
         public string GetStudentDetails()
         {
-            return $"Name={GetFullName()}; School={SchoolName}; Grade={Grade}";
+            return $"Name={GetFullName()}; School={schoolName}; Grade={grade}";
         }
 
         // SAMPLE: Class methods - calling a method from withing a method
         private string GetFullName()
         {
-            return $"{LastName}, {FirstName}";
+            return $"{lastName}, {firstName}";
         }
     }
 }
